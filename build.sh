0e4b9e28f4019515a71646ae365e61df604cafc6
jumoog <kilianvonpflugk@gmail.com>
2013-09-27 23:02:17 +0200

    Buildbot with / without Gapps

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ device=(jfltexx endeavoru m7ul i9300 i9305 t0lte t0lteatt mako toro grouper jflt
 for i in "${device[@]}"
 do
   rm -rf out/target/product/$i/*.zip*
-  sh build-probam.sh $i
-  if [ -f out/target/product/$i/probam_*.zip ]
+  sh build-probam.sh -g $i
+  if [ -f out/target/product/$i/probam_*_$i.zip ]
 	then
-		version=$(basename out/target/product/$i/probam_*.zip | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
-		scp out/target/product/$i/*.zip probam@upload.goo.im:/home/probam/public_html/$i/
-		scp out/target/product/$i/*.zip root@31.220.42.102:/var/www/
-		scp out/target/product/$i/*.zip jumoogpro@frs.sourceforge.net:/home/frs/project/probam/
+		version=$(basename out/target/product/$i/probam_*_$i.zip | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+		scp out/target/product/$i/probam_*_$i.zip probam@upload.goo.im:/home/probam/public_html/$i/
+		scp out/target/product/$i/probam_*_$i.zip root@31.220.42.102:/var/www/
+		scp out/target/product/$i/probam_*_$i-gapps.zip jumoogpro@frs.sourceforge.net:/home/frs/project/probam/
 		perl ttytter.pl -status="ProBam ${version} for ${i} is done"
 		rm -rf out/target/product/$i/*.zip*
   fi
