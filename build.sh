39459a3f9a497ea67795f65c3578c7b06221d3cf
jumoog <kilianvonpflugk@gmail.com>
2013-10-14 15:48:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ device=(jfltexx endeavoru m7ul mb886 n7100 p880 i9300 i9305 t0lte t0lteatt mako 
 for i in "${device[@]}"
 do
   rm -rf out/target/product/$i/*.zip*
-  sh build-probam.sh -g $i
+  sh build-probam.sh $i
   if [ -f out/target/product/$i/probam_*_$i.zip ]
 	then
 		version=$(basename out/target/product/$i/probam_*_$i.zip | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
