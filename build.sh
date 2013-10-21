5135f3a6861b2982d9760e0eff0cb0ade805cb40
jumoog <kilianvonpflugk@gmail.com>
2013-10-21 17:04:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-device=(jfltexx endeavoru m7ul mb886 n7100 p880 i9300 i9305 t0lte t0lteatt mako toro toroplus grouper jflteatt jfltecan jfltecri jfltespr jfltetmo jflteusc jfltevzw)
+device=(jfltexx d2att n7100 endeavoru m7ul mb886 p880 i9300 i9305 t0lte t0lteatt mako toro toroplus grouper jflteatt jfltecan jfltecri jfltespr jfltetmo jflteusc jfltevzw)
 
 for i in "${device[@]}"
 do
@@ -8,7 +8,7 @@ do
   if [ -f out/target/product/$i/probam_*_$i.zip ]
 	then
 		version=$(basename out/target/product/$i/probam_*_$i.zip | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
-		scp out/target/product/$i/probam_*_$i.zip root@162.243.33.63:/home/getcm/
+		scp out/target/product/$i/probam_*_$i.zip root@162.243.33.63:/home/upload/
 		rm -rf out/target/product/$i/*.zip*
   fi
 done
